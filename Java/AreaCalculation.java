c07ebc911b4284a4a88e4d3444fdb4a4418c6bb7
unknown <mario.rodriguez3@upr.edu>
2018-02-23 12:59:57 -0400

    Added permitter feature

--- Java/AreaCalculation.java
+++ Java/AreaCalculation.java
@@ -9,6 +9,9 @@
 		double radius =sc.nextDouble();
 
 		double area= Math.PI * (radius*radius);
-		System.out.println("The area of the circle is: "+area);
+		double halfArea= area/2;
+		System.out.println("The area of the circle is: 
+"+area);
+		System.out.println("Half of the area of the circle is: " +halfArea);
 	}
 }
