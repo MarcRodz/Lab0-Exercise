a8f420abe939339df5467f47c78de777bdf23389
unknown <mario.rodriguez3@upr.edu>
2018-02-23 13:32:51 -0400

    "Adding Feature"

--- Java/AreaCalculation.java
+++ Java/AreaCalculation.java
@@ -9,6 +9,8 @@
 		double radius =sc.nextDouble();
 
 		double area= Math.PI * (radius*radius);
+		double halfArea= area/2;
 		System.out.println("The area of the circle is: "+area);
+		System.out.print("Half of that area is: "+halfArea);
 	}
 }
